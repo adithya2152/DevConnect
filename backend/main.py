586ebd67062887b6484eb34bfe1be4955c9dd349
adithya2152 <adithyabharadwaj15@gmail.com>
2025-07-13 12:29:42 +0530

    merge fix - 3

--- backend/main.py
+++ backend/main.py
@@ -29,7 +29,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Your frontend URL
+    allow_origins=["http://localhost:5173"],  # Your frontend URL
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
